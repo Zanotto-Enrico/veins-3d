3e60e433ccdcb63438f08c2640b1e70ff6cf3043
Zanotto-Enrico <tuaemail@example.com>
2024-06-06 19:08:16 +0200

    simulation update

--- examples/lust3d/opp_runner.sh
+++ examples/lust3d/opp_runner.sh
@@ -4,13 +4,13 @@
 CONFIG_FILE="omnetpp.ini"
 
 # Numero massimo di processi in parallelo
-MAX_PARALLEL=8
+MAX_PARALLEL=14
 
 # contatore di processi
 running_processes=0
 
 # Loop per eseguire le simulazioni 
-for ((i=0; i<240; i++))
+for ((i=0; i<0; i++))
 do
     echo "Esecuzione della simulazione con -r $i"
     opp_run -m -u Cmdenv -n .:../veins:../../src/veins -c MultipleRSUs --image-path=../../images -l /usr/lib/gdal  -l ../../src/veins $CONFIG_FILE -r $i &
@@ -34,10 +34,34 @@ wait
 running_processes=0
 
 # Loop per eseguire le simulazioni
-for ((i=0; i<0; i++))
+for ((i=0; i<10; i++))
 do
     echo "Esecuzione della simulazione con -r $i"
-    opp_run -m -u Cmdenv -c OnTopOfTheBuildings -n .:../../src/veins --image-path=../../images -l ../../src/veins $CONFIG_FILE -r $i &
+    opp_run -m -u Cmdenv -c OnTopOfTheBuildings -n .:../../src/veins --image-path=../../images -l /usr/lib/gdal -l ../../src/veins $CONFIG_FILE -r $i &
+    
+    # Incrementa il contatore dei processi in esecuzione
+    ((running_processes++))
+
+    # Controlla se il numero di processi in background ha raggiunto il limite
+    if ((running_processes >= MAX_PARALLEL)); then
+        # Aspetta che uno dei processi in background termini prima di procedere
+        wait -n
+        # Decrementa il contatore dei processi in esecuzione
+        ((running_processes--))
+    fi
+done
+
+# Attendere che tutti i processi in background terminino prima di concludere lo script
+wait
+
+# contatore di processi
+running_processes=0
+
+# Loop per eseguire le simulazioni
+for ((i=0; i<10; i++))
+do
+    echo "Esecuzione della simulazione con -r $i"
+    opp_run -m -u Cmdenv -c OnCrossings -n .:../../src/veins --image-path=../../images -l /usr/lib/gdal  -l ../../src/veins $CONFIG_FILE -r $i &
     
     # Incrementa il contatore dei processi in esecuzione
     ((running_processes++))
